b9598697a0ae9de7a4ceebe7163cb44d9f002203
Maroš Varchola <marosvarchola@sunray.sk>
2022-11-15 09:44:04 +0100

    Change: little mistake

--- composer-run-lint.sh
+++ composer-run-lint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 PASS=true
-npm run lint
+var=$(composer run lint)
 if [ "$?" -eq 1 ]; then
     PASS=false
 fi
-
-if [ "$PASS" = "false" ]; then
+DIFF='begin diff'
+if [[ "$var" == *"$DIFF"* ]]; then
+  echo "have diff"
+  echo $var
+  PASS=false
+fi
+if [ "$PASS" = "false" ]; then  
     exit 1
 fi
